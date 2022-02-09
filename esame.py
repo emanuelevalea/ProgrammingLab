63094f39129c0f8c5f68c2b254b56e48e1d8b2d1
emanuelevalea <94189707+emanuelevalea@users.noreply.github.com>
2022-02-09 18:41:04 +0100

    Update esame.py

--- esame.py
+++ esame.py
@@ -1,7 +1,7 @@
 #importo "re" per estrarre dei valori in caso di linea errata
 import re 
 
-class ExamException(Exception): 
+class ExamException(Exception):
     pass
 
 class CSVTimeSeriesFile:
@@ -18,30 +18,46 @@ class CSVTimeSeriesFile:
             raise ExamException ("Errore in apertura del file")
             return None
             
-
         #creo una lista per salvare il risultato
         result = []
-        #creo una lista per salvare le date, per poi poter controllarle
+        #creo delle liste per salvare le date, per poi poter controllarle
+        years = []
+        months = []
         dates = []
         for line in openedfile:
             #creo una lista "new" che sarà l'elemento della lista "result"
             new = []
             datas = line.split(',',1)
+            error_found = False
             if (datas[0] != 'date'):
-                new.append(datas[0])
-                dates.append(datas[0])
+                
+                #controllo che la data sia scritta correttamente
+                year_month = datas[0].split("-")
+                if (len(year_month) == 2 and year_month[0].isnumeric() == True and year_month[1].isnumeric() == True):
+                    if (int(year_month[1]) >= 1 and int(year_month[1]) <= 12):
+                        new.append(datas[0])
+                        years.append(int(year_month[0]))
+                        months.append(int(year_month[1]))
+                        dates.append(datas[0])
+                else:
+                    print ("Errore riscontrato nella seguente data: {}".format(datas[0]))
+                    error_found = True
                 
                 #controllo che il dato sia un numero intero positivo, altrimenti lo salto
-                if (datas[1].strip().isnumeric() == True and int(datas[1]) > 0):
-                    new.append(int(datas[1]))
-                else:
-                    print ("Errore in conversione del valore: {}".format(datas[1])) 
-                    #se vi è un numero accompagnato da altre cose uso "re" per estrarre il numero
-                    numbers = re.findall(r'\d+', str(datas[1]))
-                    if (len(numbers) == 1 and int(numbers[0]) > 0):
-                        new.append(int(numbers[0]))
+                if (error_found == False):
+                    if (((datas[1].strip()).replace("-","",1)).isnumeric() == True):
+                        if (int(datas[1]) > 0):
+                            new.append(int(datas[1]))
+                        else:
+                            print ("Errore in conversione del valore: {}".format(datas[1]))
+                    else:
+                        print ("Errore in conversione del valore: {}".format(datas[1]))
+                        #se vi è un numero accompagnato da altre cose uso "re" per estrarre il numero
+                        numbers = re.findall(r'\d+', str(datas[1]))
+                        if (len(numbers) == 1):
+                            new.append(int(numbers[0]))
 
-                result.append(new)
+                    result.append(new)
         
         openedfile.close()
 
@@ -50,31 +66,14 @@ class CSVTimeSeriesFile:
             raise ExamException ("Il file inserito è vuoto")
             
         #controllo che il totale delle date sia un multiplo di 12
-        if (len(dates)%12 != 0 or len(dates) == 0):
+        if (len(dates)%12 != 0):
             raise ExamException ("Il totale di date non è divisibile per 12")
             
         #controllo che la stessa data non si ripeta
         for item in dates:
             if (dates.count(item) > 1):
-                raise ExamException ("La data seguente è ripetuta:{}".format(item))
-
-        #controllo che mesi e anni siano scritti correttamente
-        years = []
-        months = []
-        for item in dates:
-            dates1 = item.split("-")
-            #controllo che vi sia mese e anno soltanto
-            if (len(dates1) != 2):
-                raise ExamException ("La seguente linea è scritta erroneamente: {}".format(item))
-            #controllo che vi siano numeri
-            if (dates1[0].isnumeric() == False or dates1[1].isnumeric() == False ):
-                raise ExamException ("L'anno seguente non è corretto: {}".format(item))
-            #controllo che i mesi inseriti siano corretti
-            if (int(dates1[1]) > 12 or int(dates1[1]) < 1):
-                raise ExamException ("Il mese seguente non è corretto: {}".format(item.split("-")[1]))
-
-            years.append(int(dates1[0]))
-            months.append(int(dates1[1]))
+                raise ExamException ("La data seguente è ripetuta: {}".format(item))
+       
 
         #controllo che i mesi e gli anni siano in ordine attraverso un iteratore 
         previous_month = 0
@@ -122,6 +121,7 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
 
     #calcolo il risultato salvandolo su una lista
     result = []
+    #creo un indice per iterare su i vari anni
     year_index = 0
     year_found = False
     while(year_found == False):
@@ -129,11 +129,13 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
         year_searching = time_series[year_index][0].split("-")
         if (first_year == year_searching[0]):
             for x in range(0,12):
+                #salvo ogni risultato uguale a 0
                 result.append(0.0)
                 #creo un indice per iterare sui diversi mesi
                 month_index = x + year_index
                 #creo una lista per aggiungere ogni valore di un mese per gli anni considerati
                 month_values = []
+                
                 for i in range (int(first_year) , int(last_year) + 1):
                     if (len(time_series[month_index]) > 1):
                         month_values.append(time_series[month_index][1])
@@ -144,7 +146,6 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
                     previous_month_value = None
                     #trovo la somma delle differenze
                     for item in month_values:
-                        
                         if (previous_month_value != None):
                             result[x] = result[x] + (item - previous_month_value)
                         previous_month_value = item
